0d48997c6c9a74bdfa1315bcfa815b614d0d6cfc
Natthaphol Nirattisaikul <110712811+stupidgo11@users.noreply.github.com>
2023-09-12 00:32:59 +0700

    Update getItemsOfCurrentPage.js

--- Module1/getitemsofcurrentpage/getItemsOfCurrentPage.js
+++ Module1/getitemsofcurrentpage/getItemsOfCurrentPage.js
@@ -10,4 +10,23 @@ function getItemsOfCurrentPage(allItems, currentPage, rowsPerPage) {
   return itemsOfCurrentPages.length>0?itemsOfCurrentPages:[]
 
 }
+// function getItemsOfCurrentPage(allItems, currentPage, rowsPerPage) {
+//   //console.log(allItems)
+//   if(allItems===null||allItems===undefined) return undefined
+//   const maxPages=Math.ceil(allItems.length/rowsPerPage)
+//   //console.log(maxPages)
+
+//   if(allItems.length===0) return []
+//   if(rowsPerPage<=0) return allItems
+//   if(currentPage<=0) return allItems
+  
+//   let result=[]
+//   let lastPoint=(currentPage-1)*rowsPerPage
+//   for (let i = lastPoint; i < currentPage*rowsPerPage; i++) {
+//     if(allItems[i]===null|allItems[i]===undefined) continue
+//     result[result.length]=allItems[i]
+    
+//   }
+//   return result
+// }
 module.exports = getItemsOfCurrentPage
