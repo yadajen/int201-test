79fdaeefc49a03140397b3831b1ac2be9ceda7f6
Natthaphol Nirattisaikul <110712811+stupidgo11@users.noreply.github.com>
2023-09-12 00:36:38 +0700

    Update countCovidStatus.js

--- Module1/countcovidstatus/countCovidStatus.js
+++ Module1/countcovidstatus/countCovidStatus.js
@@ -31,4 +31,29 @@ function countCovidStatus(covidStatusArray) {
   return covidStatus;
 }
 
+// function countCovidStatus(covidStatusArray) {
+//   if(covidStatusArray===null||covidStatusArray===undefined){
+//     return undefined
+//   }
+//   if(covidStatusArray.length === 0){
+//     return {}
+//   }
+//   const obj = {}
+//   let low=0,high=0,pos=0
+//   for (let i = 0; i < covidStatusArray.length; i++) {
+//     if (covidStatusArray[i]==="negative and low risk") {
+//       low++//obj['nga]++
+//     }else if (covidStatusArray[i]==="negative and high risk") {
+//       high++
+//     }else if (covidStatusArray[i]==="positive"){
+//       pos++
+//     }
+//   }
+
+//   if(low>0) obj["negative and low risk"]=low
+//   if(high>0) obj["negative and high risk"]=high
+//   if(pos>0) obj["positive"]=pos
+  
+//   return(obj)
+// }
 module.exports = countCovidStatus
