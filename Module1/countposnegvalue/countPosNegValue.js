29f5f0f7de4a9dd0ceb74f25cf3d5373d142594e
Natthaphol Nirattisaikul <110712811+stupidgo11@users.noreply.github.com>
2023-09-12 00:35:48 +0700

    Update countPosNegValue.js

--- Module1/countposnegvalue/countPosNegValue.js
+++ Module1/countposnegvalue/countPosNegValue.js
@@ -1,31 +1,32 @@
 const { template } = require('@babel/core')
 
   function countPosNegValue(posNegIntegerArray) {
-    if (posNegIntegerArray === null || posNegIntegerArray === undefined) {
-        return undefined
-    } else if (posNegIntegerArray.length === 0) return {}
-  
+    if (posNegIntegerArray === null || posNegIntegerArray === undefined) return undefined
+    else if (posNegIntegerArray.length === 0) return {}
+    
     let len = posNegIntegerArray.length;
     let positiveCount = 0;
     let negativeCount = 0;
     
-  
     for(let i = 0; i < len; i++)
     {
-        if (posNegIntegerArray[i] > 0)
-        {
-            positiveCount++;
-        }
-        else if (posNegIntegerArray[i] < 0)
-        {
-            negativeCount++;
-        } 
-       
+        if (posNegIntegerArray[i] > 0) positiveCount++;
+        else if (posNegIntegerArray[i] < 0) negativeCount++;
     }
+    
     const count = {};
     if (positiveCount >= 0) count["positive"] = positiveCount;
     if (negativeCount >= 0) count["negative"] = negativeCount;
     return count;
-
 }
+// function countPosNegValue(posNegIntegerArray) {
+//     if(posNegIntegerArray===null||posNegIntegerArray===undefined) return undefined
+//     if(posNegIntegerArray.length === 0) return {}
+//     let Integer = {positive: 0, negative: 0}
+//     for (let i = 0; i < posNegIntegerArray.length; i++) {
+//       if(posNegIntegerArray[i]>0) Integer["positive"]++
+//       else if(posNegIntegerArray[i]<0) Integer.negative++
+//     }
+//     return Integer
+// }
 module.exports = countPosNegValue
